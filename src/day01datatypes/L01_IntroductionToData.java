6a1af81648538d33ddf3cc673ff1eaa893d72e76
aydinkeyf <adnkeyf@gmail.com>
2022-05-15 12:54:35 -0500

    first

---
+++ src/day01datatypes/L01_IntroductionToData.java
@@ -0,0 +1,12 @@
+package day01datatypes;
+
+public class L01_IntroductionToData {
+    public static void main(String[] args) {
+
+        // Notesg
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hi");
+        }
+    }
+}
