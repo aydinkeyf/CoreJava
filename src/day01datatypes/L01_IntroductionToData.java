fefcc7685b80daebeb0dc45267df3199462302ff
aydinkeyf <adnkeyf@gmail.com>
2022-05-15 13:05:51 -0500

    sample

--- src/day01datatypes/L01_IntroductionToData.java
+++ src/day01datatypes/L01_IntroductionToData.java
@@ -3,10 +3,17 @@ package day01datatypes;
 public class L01_IntroductionToData {
     public static void main(String[] args) {
 
-        // Notesg
+        // Notes
+        /*
+        git add .
+        git commit -m "data types"
+        git push
+         */
 
         for (int i = 0; i < 10; i++) {
             System.out.println("hi");
+
+
         }
     }
 }
---
+++ src/day01datatypes/Sample.java
@@ -0,0 +1,4 @@
+package day01datatypes;
+
+public class Sample {
+}
