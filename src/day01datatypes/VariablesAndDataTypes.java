fd4f78a1f5911104021a5abd55b8299ad5db9cd6
aydinkeyf <adnkeyf@gmail.com>
2022-05-16 12:21:26 -0500

    git commit -m "data types" git commit -m "data types"

--- src/day01datatypes/VariablesAndDataTypes.java
+++ src/day01datatypes/VariablesAndDataTypes.java
@@ -5,19 +5,39 @@ import java.util.concurrent.SynchronousQueue;
 public class VariablesAndDataTypes {
     public static void main(String[] args) {
         int a = 5, b = 3, c = a + b;
-        double x = 8.134124, y = 7.4132412;
-        float myPie = 1.4f, yourPie = 2.4f;
-        byte myByte = 25;
-        short mShort = 1500;
-        long nLongA = 1234567890L, nLongB = 9223372036854775807L;
-
-
         System.out.println(c);
+
+        double x = 8.134124, y = 7.4132412;
         System.out.println(x);
+
+        float myPie = 1.4f, yourPie = 2.4f;
         System.out.println(yourPie);
+
+        byte myByte = 25;
         System.out.println(myByte);
+
+        short mShort = 1500;
         System.out.println(mShort);
+
+        long nLongA = 1234567890, nLongB = 9223372036854775807L;
         System.out.println(nLongB);
+
+        char nChar = 37, cChar = 'b';
+        System.out.println(nChar);
+        System.out.println(cChar);
+
+        boolean right = true, wrong = false;
+        System.out.println(right);
+        System.out.println(wrong);
+
+
+
+
+
+
+
+
+
 
     }
 }
