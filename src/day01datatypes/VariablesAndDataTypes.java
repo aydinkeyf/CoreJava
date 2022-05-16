d5522475698aba032f45dd47e67637dad5cbeb6e
aydinkeyf <adnkeyf@gmail.com>
2022-05-16 11:36:23 -0500

    git commit -m "data types" git commit -m "data types"

--- src/day01datatypes/VariablesAndDataTypes.java
+++ src/day01datatypes/VariablesAndDataTypes.java
@@ -1,10 +1,20 @@
 package day01datatypes;
 
+import java.util.concurrent.SynchronousQueue;
+
 public class VariablesAndDataTypes {
     public static void main(String[] args) {
+        int a = 5, b = 3, c = a + b;
+        double x = 8.134124, y = 7.4132412;
+        float myPie = 1.4f, yourPie = 2.4f;
 
         byte myByte = 25;
-        System.out.print(myByte);
+        short mShort = 1500;
+        System.out.println(c);
+        System.out.println(x);
+        System.out.println(yourPie);
+        System.out.println(myByte);
+        System.out.println(mShort);
 
     }
 }
