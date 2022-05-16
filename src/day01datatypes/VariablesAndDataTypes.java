13db8d26e94b853148e19f4a5d96dd06363d2416
aydinkeyf <adnkeyf@gmail.com>
2022-05-16 08:54:54 -0500

    git commit -m "data types" git commit -m "data types"

--- src/day01datatypes/VariablesAndDataTypes.java
+++ src/day01datatypes/VariablesAndDataTypes.java
@@ -5,6 +5,9 @@ public class VariablesAndDataTypes {
         int a = 5, b = 3, c = a + b;
         System.out.print(c);
 
+        double x = 4.39008, y= 5.7653949, z = x * y;
+        System.out.print(z);
+
 
 
     }
