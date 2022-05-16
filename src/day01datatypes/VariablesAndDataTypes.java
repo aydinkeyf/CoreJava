6ba78e7bc80a371e254e03800d4c6de1f48066b6
aydinkeyf <adnkeyf@gmail.com>
2022-05-16 12:39:34 -0500

    git commit -m "data types" git commit -m "data types"

--- src/day01datatypes/VariablesAndDataTypes.java
+++ src/day01datatypes/VariablesAndDataTypes.java
@@ -5,6 +5,7 @@ import java.util.concurrent.SynchronousQueue;
 public class VariablesAndDataTypes {
     public static void main(String[] args) {
        //Starting from Int to boolean, they are primitive data types and start with lower case
+       //32 bit long and Max 2,147,483,647 , Min -2,147,483,648
         int a = 5, b = 3, c = a + b;
         System.out.println(c);
 
@@ -14,12 +15,15 @@ public class VariablesAndDataTypes {
         float myPie = 1.4f, yourPie = 2.4f;
         System.out.println(yourPie);
 
+        //8 bit long and Max 127 , Min -128
         byte myByte = 25;
         System.out.println(myByte);
 
+        // 16 bit long and Max 32,767 , Min -32,768
         short mShort = 1500;
         System.out.println(mShort);
 
+        //64 bit long and Max 32,767 , Min -32,768
         long nLongA = 1234567890, nLongB = 9223372036854775807L;
         System.out.println(nLongB);
 
